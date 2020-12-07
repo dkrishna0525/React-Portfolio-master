192545dac200b81236cbcbe645617327f562c99e
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2020-12-07 12:43:22 -0500

    add: initialCommit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Krishna D",
+    "role": "UI Developer",
+    /*"linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",*/
+    "roleDescription": " ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/krishnad0525/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dkrishna0525",
+          "className":"fa fa-github"
+        }
+        /*,
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        } */
+      ],
+    "aboutme":"My name is Krishna I'm a Front End Engineer based in Phoenix,Arizona. I describe myself as a passionate developer who loves coding, open source, and the web platform ",
+    "address":"Phoenix, AZ",
+    "website":"845-244-0473",
+    "education":[
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University, Kakinada",
+        "specialization":"Computer Science",
+        "Achievements":"Topper of the class"
+      }
+      /*,
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }*/
+    ],
+    "work":[
+      {
+        "CompanyName":"Choice Hotels International",
+        "specialization":"UI Developer/ React JS Developer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":"Environment: React, Redux, JavaScript, Express JS, Unit testing, Webpack, GitHub, and Agile Methodology."
+      },
+      {
+        "CompanyName":"T-Mobile, Bellevue, WA ",
+        "specialization":"UI Developer",
+        "MonthOfLeaving":"Feb 2018",
+        "YearOfLeaving":" – June 2019",
+        "Achievements":"Environment: HTML 5, CSS3, Sass, Less, JavaScript, Bootstrap, ReactJS, Redux, Babel, Ajax, My SQL, XML, XSL, XSLT, JSON, GitHub, Restful API, Mongo DB, Jasmine and Web Strom"
+      },
+      {
+        "CompanyName":"FTC LLC , Melbourne, FL ",
+        "specialization":"UI Developer",
+        "MonthOfLeaving":"Sep 2016 – ",
+        "YearOfLeaving":"Dec 2017",
+        "Achievements":"Environment: JavaScript, ReactJS, Redux, HTML5, CSS3, Ajax, GIT, Bootstrap, CSS, NPM, JEST, JSON."
+      }
+    ],
+    "skillsDescription":" HTML 5, CSS3, Sass, Less, JavaScript, Bootstrap, ReactJS, Redux, Babel, Ajax, MySQL, XML, XSL, XSLT, JSON, GitHub, Restful API, Mongo DB, Jasmine and Web Strom",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData

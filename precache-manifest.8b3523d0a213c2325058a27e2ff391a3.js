6c733dcf19d452eb05ffb20688886e6ea26c659c
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2020-12-07 16:11:42 -0500

    Updates

---
+++ precache-manifest.8b3523d0a213c2325058a27e2ff391a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64dd53dc90bcb2cb5160",
+    "url": "/React-Portfolio-master/static/css/main.8c14ceed.chunk.css"
+  },
+  {
+    "revision": "64dd53dc90bcb2cb5160",
+    "url": "/React-Portfolio-master/static/js/main.64dd53dc.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio-master/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ae188cb077ba5f3ea0f4",
+    "url": "/React-Portfolio-master/static/js/runtime~main.ae188cb0.js"
+  },
+  {
+    "revision": "9c592e3a1ffa320172e45ff52454193e",
+    "url": "/React-Portfolio-master/index.html"
+  }
+];

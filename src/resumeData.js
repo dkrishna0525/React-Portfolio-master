7844edb94ce970463341247ace26f9f739632d27
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2021-05-31 02:45:00 -0400

    Update-2

--- src/resumeData.js
+++ src/resumeData.js
@@ -8,12 +8,12 @@ let resumeData = {
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/krishnad0525/",
+          "url":"https://www.linkedin.com/in/krishnasrikanthdonthineni/"/*"https://www.linkedin.com/in/krishnad0525/"*/,
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"https://github.com/dkrishna0525",
+          "url":"www.github.com/krishnasrikanthdonthoineni/"/*"https://github.com/dkrishna0525"*/,
           "className":"fa fa-github"
         }
         /*,
@@ -23,14 +23,21 @@ let resumeData = {
           "className":"fa fa-twitter"
         } */
       ],
-    "aboutme":"My name is Krishna I'm a Front End Engineer based in Phoenix,Arizona. I describe myself as a passionate developer who loves coding, open source, and the web platform ",
+    "aboutme":"My name is Krishna Srikanth Donthineni I'm a Software Engineer based in New York. I describe myself as a passionate developer who loves coding, open source, and the web platform ",
     "address":"Phoenix, AZ",
-    "website":"845-244-0473",
+    "website":"623-239-7347",
     "education":[
+      {
+        "UniversityName":"State University Of New York At New Paltz",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Achievements":"Topper of the class"
+      },
       {
         "UniversityName":"Jawaharlal Nehru Technological University, Kakinada",
         "specialization":"Computer Science",
-        "Achievements":"Topper of the class"
+        "Achievements":"Passed with First Class"
       }
       /*,
       {
@@ -42,6 +49,21 @@ let resumeData = {
       }*/
     ],
     "work":[
+      {
+        "CompanyName":"Tata Consultancy Services",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2021",
+        "Achievements":"Environment: Angular, React, Redux, JavaScript, Express JS, Unit testing, Webpack, GitHub, and Agile Methodology."
+      },
+      {
+        "CompanyName":"SUNY New Paltz, New York",
+        "specialization":"Graduate Assistant",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2020",
+        "Achievements":"Environment: Java, Python, R and Data Analysis"
+      }
+      /*,
       {
         "CompanyName":"Choice Hotels International",
         "specialization":"UI Developer/ React JS Developer",
@@ -62,7 +84,7 @@ let resumeData = {
         "MonthOfLeaving":"Sep 2016 – ",
         "YearOfLeaving":"Dec 2017",
         "Achievements":"Environment: JavaScript, ReactJS, Redux, HTML5, CSS3, Ajax, GIT, Bootstrap, CSS, NPM, JEST, JSON."
-      }
+      }*/
     ],
     "skillsDescription":" HTML 5, CSS3, Sass, Less, JavaScript, Bootstrap, ReactJS, Redux, Babel, Ajax, MySQL, XML, XSL, XSLT, JSON, GitHub, Restful API, Mongo DB, Jasmine and Web Strom",
     "skills":[
@@ -103,14 +125,14 @@ let resumeData = {
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"KrishnaSrikanth worked as my GA in the 2020 Spring semester at SUNY New Paltz. He helped me with two courses that I was teaching in that semester: Business Statistics I and Introduction to Business Analytics. KrishnaSrikanth has done a great job of grading students' homework and tutoring. KrishnaSrikanth provided timely responses to students' questions and I have received good feedback from students on his tutorial help.",
+        "name":"Tao Li Assistant Professor in the school of business at SUNY New Paltz"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Krishna is one of the outstanding students in our Master's program in Computer Science.He has a thorough foundation in CS, is a fast learner, and displays a keen eagerness to learn new things.I have supervised him for two years and his work been exemplary.He will be an asset to any organization.",
+        "name":"Anthony Denizard, MSCS Adjunct Instructor at SUNY New Paltz"
       }
-    ]
+    ] 
   }
   
   export default resumeData
